9a10e5a85882a750865f45acb928df9d980ac0e3
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-09-14 12:19:22 +0300

    Views, URLs, Serializers

--- tusome/tusome_api/views.py
+++ tusome/tusome_api/views.py
@@ -1,3 +1,10 @@
-from django.shortcuts import render
+from rest_framework import generics
+from tusome_core.models import Book, Review
+from .serializers import BookSerializer
 
-# Create your views here.
+class BookList(generics.ListCreateAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+class BookDetail(generics.RetrieveUpdateDestroyAPIView):
+    pass

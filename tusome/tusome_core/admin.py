26255b3bb50f7e4e8d96db50421f44a816515c67
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-09-23 15:25:41 +0300

    Model registration

--- tusome/tusome_core/admin.py
+++ tusome/tusome_core/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
-
+from .models import Book, Review, Category
 # Register your models here.
+admin.site.register(Book)
+admin.site.register(Category)
+admin.site.register(Review)

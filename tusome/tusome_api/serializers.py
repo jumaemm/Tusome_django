a516e511716621167eeee31bea1b78628afc9550
hazy254 <jumaemmanuel.EJ@outlook.com>
2022-09-23 15:25:03 +0300

    Uploaded By field

--- tusome/tusome_api/serializers.py
+++ tusome/tusome_api/serializers.py
@@ -4,4 +4,4 @@ from tusome_core.models import Book, Review
 class BookSerializer(serializers.ModelSerializer):
     class Meta:
         model = Book
-        fields = ('title', 'isbn', 'author', 'cover')
+        fields = ('title', 'isbn', 'author', 'cover', 'category', 'uploaded_by')
